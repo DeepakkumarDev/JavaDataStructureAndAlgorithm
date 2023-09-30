3016002cd8747cc5d99baa83e7f428ef4def711c
Deepak Kumar Dev <deepak.dev.mca22.du@gmail.com>
2023-10-01 01:08:12 +0530

    JavaProgram

---
+++ ArrayTwoDimensional.java
@@ -0,0 +1,17 @@
+public class ArrayTwoDimensional
+{
+	public static void main(String[] args)
+	{
+		int B[][]={{1,2,3},{4,5,6},{7,8,9}};
+		
+		 for(int X[]:B)
+		 {
+		 	for (int y:X)
+		 	{
+		 		System.out.print(" "+y);
+		 	}
+		 	System.out.println("");
+
+		 }
+	}
+}
